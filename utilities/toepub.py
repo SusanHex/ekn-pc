29d4679322691230965ae8c62b781cbd1bba52c0
= <=>
2023-12-01 13:08:06 -0700

    created book to epub script file and base code

---
+++ utilities/toepub.py
@@ -0,0 +1,10 @@
+from sys import argv
+from re import compile, match
+
+def main():
+    # open story file
+    with open(argv[1], 'r', encoding='utf-8') as story_file:
+        pass
+
+if __name__ == '__main__':
+    main()
